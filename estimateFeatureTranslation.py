6462a068a9393bdfe7cc3f28b184731e14da6660
Rajiv Patel-O'Connor <rajiv.patel.oconnor@gmail.com>
2017-11-20 15:50:07 -0500

    added rgb2gray in helper.py and untested estimateFeatureTranslation

--- estimateFeatureTranslation.py
+++ estimateFeatureTranslation.py
@@ -1,9 +1,13 @@
 '''
   File name: estimateFeatureTranslation.py
-  Author:
-  Date created:
+  Author: Rajiv Patel-O'Connor
+  Date created: 11-20-2017
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp2d
+from helper import rgb2gray
 '''
   File clarification:
     Estimate the translation for single features 
@@ -19,4 +23,40 @@
 
 def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
   #TODO: Your code here
+  
+  #Convert from RGB to grayscale
+  gray1 = rgb2gray(img1)
+  gray2 = rgb2gray(img2)
+  
+  # Get size of image and allocate space for matrices in "culmination"
+  [h, w] = gray1
+  LHS_summation = np.zeros((2,2)) #make this an np.empty after shown to work
+  RHS_summation = np.zeros((2, 1))
+  
+  # Get partial with respect to time
+  It = gray2 - gray1
+  
+  # Calculate boundaries of window (normally 10x10 except when on boundary)
+  windowMinX = startX - 4 if startX - 4 > 0 else startX
+  windowMaxX = startX + 5 if startX + 5 < w else w - startX
+  windowMinY = startY - 4 if startY - 4 > 0 else startY
+  windowMaxY = startY + 5 if startY + 5 < h else h - startY
+  
+  # Get window points from Ix, Iy, and It
+  Ix_window = Ix[windowMinY: windowMaxY, windowMinX: windowMaxX + 1]
+  Iy_window = Iy[windowMinY: windowMaxY, windowMinX: windowMaxX]
+  It_window = It[windowMinY: windowMaxY, windowMinX: windowMaxX]
+  
+  #Fill in culmination matrices
+  LHS_summation[0][0] = np.sum(np.multiply(Ix_window, Ix_window))
+  LHS_summation[0][1] = LHS_summation[1][0] = np.sum(np.multiply(Ix_window, Iy_window))
+  LHS_summation[1][1] = np.sum(np.multiply(Iy_window, Iy_window))
+  
+  RHS_summation[0][0] = -1.0 * np.sum(np.multiply(Ix_window, It_window))
+  RHS_summation[1][0] = -1.0 * np.sum(np.multiply(Iy_window, It_window))
+  
+  #Solve for newX and newY
+  LHS_inverse = np.linalg.inv(LHS_summation)
+  newX, newY = LHS_inverse.dot(RHS_summation).T[0]
+  
   return newX, newY
--- helper.py
+++ helper.py
@@ -3,9 +3,12 @@
   Author:
   Date created:
 '''
-
+import numpy as np
 '''
   File clarification:
   Include any helper function you want for this project such as the 
   video frame extraction, video generation, drawing bounding box and so on.
 '''
+
+def rgb2gray(img):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
