626abb0a911c882bb8edd2a1fda3d070293ca1de
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-11-20 20:00:21 -0500

    Added ability to detect "side" faces, but it is commented out bc TA instructed that it is not required
    Added ability to detect "side" faces, but it is commented out bc TA
    instructed that it is not required

--- detectFace.py
+++ detectFace.py
@@ -21,12 +21,21 @@ def detectFace(img):
     #returns (x, y, w, h) for each face in the pic
     faces = faceCascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
                                        flags = cv2.CASCADE_SCALE_IMAGE)
+    
+    #If we want to detect side-profile faces, aka faces that are not facing forward
+    #then uncomment the below lines
+    #faceCascade_side = cv2.CascadeClassifier('haarcascade_profileface.xml')
+    #faces_side = faceCascade_side.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+    #                                   flags = cv2.CASCADE_SCALE_IMAGE)
+    #faces = faces + faces_side
+    
 
     #if we want to visualize the faces
     #for (x, y, w, h) in faces:
     #    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     #cv2.imshow('image', img)
     #cv2.waitKey(0)
+
 
     #parse this output into the F x 4 x 2 matrix that we want
     bbox = np.zeros([np.size(faces,0),4,2], np.int)
